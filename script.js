5fe6a8f163e3d8b80ec9d7dab9c268dadadcf038
Steve Babb <steve.babb@outlook.com>
2021-01-06 12:39:24 -0600

    pull all my working html and js code from WorkingDiv folder and overwrote main index and script files

--- script.js
+++ script.js
@@ -1,284 +1,226 @@
 
-// var body = document.body;
 
-//accessing gameStart div and elements within
-var beginGameDiv = document.querySelector("#gameStart");
-var startBtn = document.querySelector("#startUp");
+        // Accessing html elements
+        var welcomeScreenDiv = document.querySelector("#welcomeScreen");
+        var theQuizDiv = document.querySelector("#theQuiz");
+        var startPlayBtn = document.querySelector("#startPlay");
+        var showQuestionDiv = document.querySelector("#showQuestion");
+        var showAnswersDiv = document.querySelector("#showAnswers");
+        var theTimerDiv = document.querySelector("#theTimer");
+        var theGameOverDiv = document.querySelector("#gameOver");
+        var theUserInitials = document.querySelector("#userInitials");
+        var theFinalScore = document.querySelector("#finalScore");
+        var leaderBoardDiv = document.querySelector("#leaderBoard");
+        var timeRemainingDiv = document.querySelector("#timeRemaining");
+        var theLeaderList = document.querySelector("#leaderList");
+        var linkToLeaderBoardBtn = document.querySelector("#linkToLeaderBoard");
 
-// accessing questionSession div and elements within
-var showQuestionDiv = document.querySelector("#questionSession");
-var theQuestion = document.querySelector("#theQuestion");
-var potentialAnswers = document.querySelector("#answerSet");
-var validateAnswer = document.querySelector("#validate");
-var TimerDiv = document.querySelector("#theTimer");
-var timeRmaining = document.querySelector("#timeRemaining");
+        //Creating a new elements
+        var theQuestionEl = document.createElement("h2");
+        var validatedEl = document.createElement("p");
 
+        // //creating new GameOver div elements
 
-//accessing gameOver div and elements
-var gameOverDiv = document.querySelector("#gameOver");
-var yourScoreSum = document.querySelector("#yourScore");
-var userInitialsInput = document.querySelector("#userInitials");
-var postScoreBtn = document.querySelector("#postScore");
-var noInitialsMsg = document.querySelector("#invalidResponse");
-var finalScore = document.querySelector("#finalScore");
+        var invalidResponseEl = document.querySelector("#invalidResponse");
+        var postScoreBtn = document.querySelector("#postScore");
+        var finalScoreEl = document.querySelector("#finalScore");
 
-//accessing leaderBoard div and elements
-var leaderBoardDiv = document.querySelector("#leaderBoard");
-var startOverBtn = document.querySelector("#startOver");
-var leaderList = document.querySelector("#leaderList");
-
-
+        //appending content in GameOver div
 
 
-// Global variable declarations
-var userInitials;
-var allScores = []
-var answerCheck;
-var theirAnswer;
-var timeToPlay = 10;
-var timeSeconds = timeToPlay;
-var score = 0;
+        // setting global variables
+        var answerBtn;
+        var score = 0;
+        var validated;
+        var timeToPlay = 60;
+        var timeSeconds = timeToPlay;
+        var allScores = [];
+        var userInitials;
+        var questionIndex = 0;
 
-var j = 0;
+        var QandA = [{
+            question: "Who is the Director of the National Institute of Allergy and Infectious Diseases?",
+            answers: ["Dr. Anthony Fauchi", "Dr. Debra Birx", "Dr. Anthony Fauci", "Dr. Debra Berx"],
+            correct: "Dr. Anthony Fauci"
+        },
+        {
+            question: "Which of the following is a city in China?",
+            answers: ["Woohan", "Beijhing", "Shanghai", "Boston"],
+            correct: "Shanghai"
+        },
+        {
+            question: "Which of the following pharmaceutical companies was the first to get a Covid 19 vaccine approved in US?",
+            answers: ["Moderna", "Phfizer", "Pfizer", "Medarna"],
+            correct: "Pfizer"
+        },
+        {
+            question: "Who is Joe Biden's Vice President (elect)",
+            answers: ["Kamala Harris", "Camala Harris", "Kammala Harris", "Kamala Haris"],
+            correct: "Kamala Harris"
+        },
+        {
+            question: "Which active ingredient in cannibis is believed to treat pain and anxiety without a 'high'?",
+            answers: ["CDC", "THC", "TBD", "CBD"],
+            correct: "CBD"
+        },
+        {
+            question: "She is married to Kanye West...",
+            answers: ["Kim Jon-un", "Cloe Kardashian", "Kim Kardashian", "Cloey Kardashian"],
+            correct: "Kim Kardashian"
+        },
+        {
+            question: "What country used to be part of the Soviet Union?",
+            answers: ["Lathvia", "Kyrgystan", "Armania", "Siberia"],
+            correct: "Kyrgystan"
+        },
+        {
+            question: "Which of these people is a world famous golfer?",
+            answers: ["Jack Nicolson", "Tiger Wood", "Arnold Paulmer", "Gary Player"],
+            correct: "Gary Player"
+        },];
 
-//creating new GameOver div elements
-var h2Success = document.createElement("h2");
-var h2Fail = document.createElement("h2");
+        window.onload = function () {
+            welcomeScreenDiv.style = "border: 2px solid black;";
+            leaderBoardDiv.style = "display: none;";
+            theQuizDiv.style = "display: none;";
+            theGameOverDiv.style = "display: none;";
 
-//appending content in GameOver div
-h2Success.textContent = "Thank you!";
-h2Fail.textContent = "You must enter initials";
-h2Success.setAttribute("style", "color:black; font-size: 14px;");
-h2Fail.setAttribute("style", "color:black; font-size: 14px;");
+        }
 
-//accessing leaderBoard div elements
-var leadersDiv = document.querySelector("#leaders");
-var startOverBtn = document.querySelector("#startOver");
-
-//creating new leadBoard div elements
-var first = document.createElement("li");
-var second = document.createElement("li");
-var third = document.createElement("li");
-var fourth = document.createElement("li");
-var fifth = document.createElement("li");
-
-
-//setting initial game score
-var score = 0;
-
-
-//object that contains questions, potential answers, and correct answers
-var QandA = [{
-    question: "Who is the Director of the National Institute of Allergy and Infectious Diseases?",
-    answers: ["Dr.Anthony Fauchi", "Dr. Debra Birx", "Dr. Anthony Fauci", "Dr. Debra Berx"],
-    correct: "Dr. Anthony Fauci"
-},
-{
-    question: "Which of the following is a city in China?",
-    answers: ["Woohan", "Beijhing", "Shanghai", "Boston"],
-    correct: "Shanghai"
-},
-{
-    question: "Which of the following pharmaceutical companies was the first to get a Covid 19 vaccine approved in US?",
-    answers: ["Moderna", "Phfizer", "Pfizer", "Medarna"],
-    correct: "Pfizer"
-},
-{
-    question: "Who is Joe Biden's Vice President (elect)",
-    answers: ["Kamala Harris", "Camala Harris", "Kammala Harris", "Kamala Haris"],
-    correct: "Kamala Harris"
-},
-{
-    question: "Which active ingredient in cannibis is believed to treat pain and anxiety without a 'high'?",
-    answers: ["CDC", "THC", "TBD", "CBD"],
-    correct: "CBD"
-},
-{
-    question: "She is married to Kanye West...",
-    answers: ["Kim Jon-un", "Cloe Kardashian", "Kim Kardashian", "Cloey Kardashian"],
-    correct: "Kim Kasrdashian"
-},
-{
-    question: "What country used to be part of the Soviet Union?",
-    answers: ["Lathvia", "Kyrgystan", "Armania", "Siberia"],
-    correct: "Kyrgystan"
-},
-{
-    question: "Which of these people is a world famous golfer?",
-    answers: ["Jack Nicolson", "Tiger Wood", "Arnold Paulmer", "Gary Player"],
-    correct: "Gary Player"
-},];
-
-console.log(QandA[1].question, QandA[1].answers[2]);
-console.log(QandA.length);
-
-//ensures that the beginGame div is all that loads when page loads.
-window.onload = function () {
-    beginGameDiv.setAttribute("class", "showDiv");
-    showQuestionDiv.setAttribute("class", "hideDiv");
-    gameOverDiv.setAttribute("class", "hideDiv");
-    leaderBoardDiv.setAttribute("class", "hideDiv");
-    TimerDiv.setAttribute("class", "hideDiv");
-
-
-    init();
-}
-
-//accessing stored users
-function init() {
-    var storedGamers = JSON.parse(localStorage.getItem("allGamers"));
-    if (storedGamers !== null) {
-        allScores = storedGamers;
-    }
-}
-
-//listening for game start, when button clicked, switches to showQuestions div
-startBtn.addEventListener("click", function () {
-    showQuestions();
-})
-
-//Begin showing questions
-console.log(QandA.length);
-
-
-function showQuestions() {
-    startTimer();
-
-    //display the show question Div
-    beginGameDiv.setAttribute("class", "hideDiv");
-    showQuestionDiv.setAttribute("class", "showDiv");
-    gameOverDiv.setAttribute("class", "hideDiv");
-    leaderBoardDiv.setAttribute("class", "hideDiv");
-    TimerDiv.setAttribute("class", "showDiv")
-
-
-    // for (j= 0; j < QandA.length; j++) {
-
-    theQuestion.append(QandA[j].question);
-
-
-    QandA[j].answers.forEach(function (answers) {
-        var answerBtnEl = document.createElement("button");
-        answerBtnEl.textContent = answers;
-        potentialAnswers.appendChild(answerBtnEl);
-        var breakPoint = document.createElement("br");
-        potentialAnswers.appendChild(breakPoint);
-
-
-        answerBtnEl.addEventListener("click", function () {
-
-
-            if (answerBtnEl.textContent === QandA[j].correct) {
-                answerCheck = "Correct";
-                validateAnswer.append(answerCheck);
-                score++;
+        init();
+        function init() {
+            var storedGamers = JSON.parse(localStorage.getItem("allGamers"));
+            if (storedGamers !== null) {
+                allScores = storedGamers;
             }
-            else {
-                answerCheck = "Incorrect";
-                validateAnswer.append(answerCheck);
-            }
-console.log(score);
-console.log(answerCheck)
-            postQuestionPause();
-            goToGameOver();
+            console.log(allScores);
+        }
 
+        linkToLeaderBoardBtn.addEventListener("click", function () {
+            goToLeaderBoard();
         })
-    })
-            
-            // theQuestion.textContent.remove();
-            // potentialAnswers.textContent.remove();
-    
+        // function init() {
 
-            
+        // }
 
-}
+        startPlayBtn.addEventListener("click", function () {
+            quizQuestion();
+            startTimer();
+        })
+        // quizQuestion();
 
-
-            console.log(QandA[j]);
-
-
-
-
-
+        // Show questions and answers and record response
+        function quizQuestion() {
+            theQuizDiv.style = "display: block; border: 2px solid black";
+            leaderBoardDiv.style = "display: none";
+            welcomeScreenDiv.style = "display: none";
 
 
-// Timer function
-function startTimer() {
-    interval = setInterval(function () {
+            showQuestion = (QandA[questionIndex].question)
+            theQuestionEl.textContent = showQuestion;
+            showQuestionDiv.append(theQuestionEl);
 
-        if (timeToPlay > 0) {
-            timeSeconds--;
-            timeRemaining.textContent = timeSeconds;
+            QandA[questionIndex].answers.forEach(function (answers) {
+                var answerBtn = document.createElement("button");
+                answerBtn.textContent = answers;
+                showAnswersDiv.appendChild(answerBtn);
+                answerBtn.setAttribute("style", "width: 200px;");
+                var breakPoint = document.createElement("br");
+                showAnswersDiv.appendChild(breakPoint);
+
+
+                answerBtn.addEventListener("click", function () {
+                    console.log(answerBtn.textContent);
+                    if (answerBtn.textContent === QandA[questionIndex].correct) {
+                        score++;
+                        console.log(score);
+                        validatedEl.textContent = "Correct! Your score is " + score + "."
+                        showAnswersDiv.append(validatedEl);
+                    }
+                    else {
+                        validatedEl.textContent = "Incorrect.  Your score is " + score + " ...AND lost 10 seconds on the timer";
+                        showAnswersDiv.append(validatedEl);
+                        timeSeconds -= 10;
+                    }
+                    questionIndex++;
+                    setTimeout(function () {
+                        if (questionIndex < 8) {
+                            showQuestionDiv.innerHTML = "";
+                            showAnswersDiv.innerHTML = "";
+
+                            quizQuestion();
+                        }
+                        else {
+                            gameOver();
+                        }
+                    }, 1500);
+
+
+                })
+            })
         }
-        else {
-            timeRemaining.textContent = "0";
 
-            clearInterval(interval);
-            gameOverDiv();
+        // Timer function
+        function startTimer() {
+
+            interval = setInterval(function () {
+
+
+                if (timeSeconds > 0) {
+                    timeSeconds--;
+                    timeRemainingDiv.textContent = timeSeconds;
+                }
+                else {
+                    clearInterval(interval);
+                    var timeExpired = document.createElement("p");
+                    timeExpired.textContent = "Time Expired";
+                    showAnswersDiv.append(timeExpired);
+                    gameOver();
+                }
+            }, 1000);
         }
-    }, 1000);
-}
 
-// Pause function
-function postQuestionPause() {
-    interval = setInterval(function () {
-        questionDiv.remove();
-    }, 1000);
-}
+        // GameOver div control
+        function gameOver() {
+            theQuizDiv.style = "display: none";
+            theGameOverDiv.style = "border: 2px solid black;"
 
-function postGameOverPause() {
-    interval = setInterval(function () {
-        gameOverDiv.remove();
-        goToLeaderBoard();
-    }, 3000);
-}
+            finalScoreEl.textContent = "Your final score is " + score;
 
 
-//  Leaderboard div control
-function goToLeaderBoard() {
-    beginGameDiv.setAttribute("class", "hideDiv");
-    showQuestionDiv.setAttribute("class", "hideDiv");
-    gameOverDiv.setAttribute("class", "hideDiv");
-    leaderBoardDiv.setAttribute("class", "showDiv");
+            postScoreBtn.addEventListener("click", function (event) {
 
-    //create object to hold userintials and score
-    var scoreObject = { gamer: userInitials, scoreValue: score }
+                userInitials = document.querySelector("#userInitials").value;
 
-    //adding scoreObject to the allScores array
-    allScores.push(scoreObject);
+                if (userInitials === "") {
 
-    localStorage.setItem("allGamers", JSON.stringify(allScores));
-    first.textContent = userInitials + "scored " + score;
-    leaderList.appendChild(first);
+                    invalidResponseEl.textContent = "You need to enter initials";
+                }
+                else {
+                    //create object to hold userintials and score
+                    var scoreObject = { gamer: userInitials, scoreValue: score }
 
-
-}
-
-// GameOver div control
-function goToGameOver() {
-    beginGameDiv.setAttribute("class", "hideDiv");
-    showQuestionDiv.setAttribute("class", "hideDiv");
-    gameOverDiv.setAttribute("class", "showDiv");
-    leaderBoardDiv.setAttribute("class", "hideDiv");
-    postScoreBtn.addEventListener("click", function (event) {
-        // event.preventDefault();
-
-        userInitials = document.querySelector("#userInitials").value;
-
-        if (userInitials === "") {
-
-            noInitialsMsg.appendChild(h2Fail);
-        }
-        else {
-            h2Fail.remove();
-            noInitialsMsg.appendChild(h2Success);
+                    //adding scoreObject to the allScores array
+                    allScores.push(scoreObject);
+                    invalidResponseEl.textContent = "Great";
+                    goToLeaderBoard();
+                }
+            })
 
         }
-        finalScore.textContent = "Your final score is " + score;
-        finalScore.append(score);
-        postGameOverPause();
 
-    });
-}
+        function goToLeaderBoard() {
+            theGameOverDiv.style = "display: none";
+            leaderBoardDiv.style = "boarder: 2px solid black";
+            welcomeScreenDiv.style = "display: none";
 
+            localStorage.setItem("allGamers", JSON.stringify(allScores));
 
+            for (var i = 0; i < allScores.length; i++) {
+
+                var newEntry = document.createElement("li")
+                newEntry.textContent = allScores[i].gamer + " - " + allScores[i].scoreValue;
+                theLeaderList.append(newEntry);
+            };
+
+        }
